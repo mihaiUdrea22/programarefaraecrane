264d2702a22f35c453775cde6cd0f7c75311101b
araripeit <araripe.it@tech-center.com>
2025-02-26 21:33:51 +0200

    solve share image issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     siteName: "Programare Fără Ecrane",
     images: [
       {
-        url: "https://www.programarefaraecrane.ro/images/programarefaraecrane.png",
+        url: "https://www.programarefaraecrane.ro/programarefaraecrane.png",
         width: 1200,
         height: 630,
         alt: "Programare Fără Ecrane - Învățare Interactivă pentru Copii",
@@ -34,7 +34,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "Programare Fără Ecrane",
     description: "Învățare interactivă pentru copii prin jocuri educative fără ecrane!",
-    images: ["https://www.programarefaraecrane.ro/images/programarefaraecrane.png"],
+    images: ["https://www.programarefaraecrane.ro/programarefaraecrane.png"],
   },
 };
 
@@ -49,7 +49,7 @@ export default function RootLayout({
         {/* Meta tags Open Graph */}
         <meta property="og:title" content="Programare Fără Ecrane - Învățare Interactivă pentru Copii" />
         <meta property="og:description" content="Ajută-l pe copilul tău să-și dezvolte gândirea logică prin activități interactive și educative fără ecrane!" />
-        <meta property="og:image" content="https://www.programarefaraecrane.ro/images/programarefaraecrane.png" />
+        <meta property="og:image" content="https://www.programarefaraecrane.ro/programarefaraecrane.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:url" content="https://www.programarefaraecrane.ro" />
@@ -59,7 +59,7 @@ export default function RootLayout({
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Programare Fără Ecrane - Învățare Interactivă pentru Copii" />
         <meta name="twitter:description" content="Învățare interactivă pentru copii prin jocuri educative fără ecrane!" />
-        <meta name="twitter:image" content="https://www.programarefaraecrane.ro/images/programarefaraecrane.png" />
+        <meta name="twitter:image" content="https://www.programarefaraecrane.ro/programarefaraecrane.png" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
